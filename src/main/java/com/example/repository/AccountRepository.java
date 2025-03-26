7d8e0201824d4d90da3140b9c55afa89e038369d
Amanda Hauer <arh4uwe@virginia.edu>
2025-03-26 21:41:26 +0000

    Completed AccountRepository and added @RestController annotation to SocialMediaController

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,24 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.entity.Account;
+
+public interface AccountRepository extends CrudRepository<Account, Integer> {
+    /**
+	 * Returns whether an entity with the given username exists.
+	 *
+	 * @param username the username to search for
+	 * @return {@literal true} if an entity with the given username exists, {@literal false} otherwise.
+	 */
+    boolean existsByUsername(String username);
+
+    /**
+	 * Returns whether an entity with the given username and password exists.
+	 *
+	 * @param username the username to search for
+     * @param password the password to search for
+	 * @return {@literal true} if an entity with the given username and password exists, {@literal false} otherwise.
+	 */
+    boolean existsByUsernameAndPassword(String username, String password);
 }
