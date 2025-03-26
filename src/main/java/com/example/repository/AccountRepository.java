c6ceca240473c869b1274cc6397c7c42509599ab
Amanda Hauer <arh4uwe@virginia.edu>
2025-03-26 21:48:38 +0000

    Completed MessageRepository and updated comments in AccountRepository

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -6,19 +6,19 @@ import com.example.entity.Account;
 
 public interface AccountRepository extends CrudRepository<Account, Integer> {
     /**
-	 * Returns whether an entity with the given username exists.
+	 * Returns whether an account with the given username exists.
 	 *
 	 * @param username the username to search for
-	 * @return {@literal true} if an entity with the given username exists, {@literal false} otherwise.
+	 * @return {@literal true} if an account with the given username exists, {@literal false} otherwise.
 	 */
     boolean existsByUsername(String username);
 
     /**
-	 * Returns whether an entity with the given username and password exists.
+	 * Returns whether an account with the given username and password exists.
 	 *
 	 * @param username the username to search for
      * @param password the password to search for
-	 * @return {@literal true} if an entity with the given username and password exists, {@literal false} otherwise.
+	 * @return {@literal true} if an account with the given username and password exists, {@literal false} otherwise.
 	 */
     boolean existsByUsernameAndPassword(String username, String password);
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,15 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.entity.Message;
+
+public interface MessageRepository extends CrudRepository<Message, Integer> {
+    /**
+	 * Returns whether a message posted by the user with the given account ID exists.
+	 *
+	 * @param username the account ID to search for
+	 * @return {@literal true} if a message posted by the user with the given account ID exists, {@literal false} otherwise.
+	 */
+    Iterable<Message> findByPostedBy(Integer postedBy);
 }
