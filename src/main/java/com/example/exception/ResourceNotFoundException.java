db3487682e78a8ded0e780398cd7631b42a5c4fa
Amanda Hauer <arh4uwe@virginia.edu>
2025-03-27 02:04:03 +0000

    ResourceNotFoundException now extends RuntimeException

--- src/main/java/com/example/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.exception;
 /**
  * An exception to be thrown if a service tries to find a resource, but cannot find it.
  */
-public class ResourceNotFoundException extends Exception {
+public class ResourceNotFoundException extends RuntimeException {
     /**
      * Creates a new ResourceNotFoundException with the given message.
      * 
